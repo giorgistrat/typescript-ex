9978ede577ecb84bfb06482b75ada1d6a861d91d
Giorgi Beriashvili <Giorgi_Beriashvili@epam.com>
2023-10-02 21:58:42 +0400

    add subclasses

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,74 @@
 class Department {
-  name: string;
+  protected employees: string[] = []; // Protected is available in this class and also for the classes that extends Department.
+  // But Private doesn't
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, private name: string) {
+    // this.name = name;
+    // this.id = id;
   }
 
   describe(this: Department) {
-    console.log('Deparment: ' + this.name);
+    console.log(`Department (${this.id}): (${this.name})`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
-accounting.describe();
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+class AccountingDepartment extends Department {
+  reports: string[];
+  constructor(id: string, reports: string[]) {
+    super(id, 'Accounting');
+    this.reports = reports;
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// IT
+const it = new ITDepartment('d1', ['Max']);
+it.describe();
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+it.printEmployeeInformation();
+
+// ACCOUNTING
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Something went wrong...');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+accounting.printReports();
+accounting.printEmployeeInformation();
