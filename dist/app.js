ab8492257db5f9a840e00714be7622a226072f5e
Giorgi Beriashvili <Giorgi_Beriashvili@epam.com>
2023-10-02 18:22:53 +0400

    ts compiler to es5

--- dist/app.js
+++ dist/app.js
@@ -12,3 +12,26 @@ var testFunc = function (num1, num2) {
 button.addEventListener('click', function () {
     return clickHandler.bind(null, "You're Welcome!");
 });
+var hobbies = ['Sports', 'Cooking'];
+var activeHobbies = ['Hiking'];
+activeHobbies.push.apply(activeHobbies, hobbies);
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (acc, item) {
+        return acc + item;
+    }, 0);
+};
+var addedNumbers = add(231, 321);
+console.log('addedNumbers ' + addedNumbers);
+var person = {
+    firstName: 'Giorgi',
+    age: 23,
+};
+var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
+console.log(hobbies, hobby1, hobby2);
+var userName = person.firstName, age = person.age;
+console.log(userName, age);
+//# sourceMappingURL=app.js.map
