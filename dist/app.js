788bb5cb0ed9f2162bfb03d5fd24fd039a7bcd3f
Giorgi Beriashvili <Giorgi_Beriashvili@epam.com>
2023-10-02 19:04:48 +0400

    ts class syntax

--- dist/app.js
+++ dist/app.js
@@ -1,37 +1,15 @@
 "use strict";
-var button = document.querySelector('button');
-function clickHandler(message) {
-    console.log('Clicked! ' + message);
-}
-var testFunc = function (num1, num2) {
-    if (num1 > num2) {
-        return true;
+var Department = (function () {
+    function Department(n) {
+        this.name = n;
     }
-    return false;
-};
-button.addEventListener('click', function () {
-    return clickHandler.bind(null, "You're Welcome!");
-});
-var hobbies = ['Sports', 'Cooking'];
-var activeHobbies = ['Hiking'];
-activeHobbies.push.apply(activeHobbies, hobbies);
-var add = function () {
-    var numbers = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        numbers[_i] = arguments[_i];
-    }
-    return numbers.reduce(function (acc, item) {
-        return acc + item;
-    }, 0);
-};
-var addedNumbers = add(231, 321);
-console.log('addedNumbers ' + addedNumbers);
-var person = {
-    firstName: 'Giorgi',
-    age: 23,
-};
-var hobby1 = hobbies[0], hobby2 = hobbies[1], remainingHobbies = hobbies.slice(2);
-console.log(hobbies, hobby1, hobby2);
-var userName = person.firstName, age = person.age;
-console.log(userName, age);
+    Department.prototype.describe = function () {
+        console.log('Deparment: ' + this.name);
+    };
+    return Department;
+}());
+var accounting = new Department('Accounting');
+accounting.describe();
+var accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,17 @@
-const button = document.querySelector('button')!;
+class Department {
+  name: string;
 
-function clickHandler(message: string) {
-  console.log('Clicked! ' + message);
-}
-
-const testFunc = (num1: number, num2: number): boolean => {
-  if (num1 > num2) {
-    return true;
+  constructor(n: string) {
+    this.name = n;
   }
 
-  return false;
-};
+  describe(this: Department) {
+    console.log('Deparment: ' + this.name);
+  }
+}
 
-button.addEventListener('click', () =>
-  clickHandler.bind(null, "You're Welcome!")
-);
+const accounting = new Department('Accounting');
+accounting.describe();
 
-const hobbies: string[] = ['Sports', 'Cooking'];
-const activeHobbies: string[] = ['Hiking'];
-
-activeHobbies.push(...hobbies);
-
-// Functions
-const add = (...numbers: number[]): number => {
-  return numbers.reduce((acc, item) => {
-    return acc + item;
-  }, 0);
-};
-
-const addedNumbers = add(231, 321);
-console.log('addedNumbers ' + addedNumbers);
-
-type PersonTypes = {
-  firstName: string;
-  age: number;
-};
-
-const person: PersonTypes = {
-  firstName: 'Giorgi',
-  age: 23,
-};
-
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-console.log(userName, age);
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+accountingCopy.describe();
